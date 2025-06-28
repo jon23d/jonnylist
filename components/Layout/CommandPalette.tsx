dcc98f6291dc0456a8c1c9bd4476dddaac0f3e4a
Jonathon Deason <jon23d@gmail.com>
2025-06-28 09:00:42 -0400

    add ability to search for tasks in spotlight

--- components/Layout/CommandPalette.tsx
+++ components/Layout/CommandPalette.tsx
@@ -1,35 +1,91 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
 import { IconManualGearboxFilled, IconSearch, IconSettings } from '@tabler/icons-react';
-import { Spotlight, SpotlightActionData } from '@mantine/spotlight';
+import { Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Spotlight, SpotlightActionData, SpotlightActionGroupData } from '@mantine/spotlight';
+import TaskEditor from '@/components/Tasks/TaskEditor';
 import { useDataSource } from '@/contexts/DataSourceContext';
+import { Task, TaskStatus } from '@/data/documentTypes/Task';
 
 export default function CommandPalette() {
   const router = useRouter();
   const dataSource = useDataSource();
   const [contexts, setContexts] = useState<string[]>([]);
+  const [openTasks, setOpenTasks] = useState<Task[]>([]);
+  const [taskOpened, { open, close }] = useDisclosure(false);
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
 
-  const actions: SpotlightActionData[] = [
-    {
-      id: 'settings',
-      title: 'Settings',
-      description: 'Open settings',
-      onClick: () => router.push('/settings'),
-      leftSection: <IconSettings size={24} stroke={1.5} />,
-    },
-  ];
-  contexts.forEach((context) => {
-    actions.push({
+  const cancelEditing = () => {
+    setSelectedTask(null);
+    close();
+  };
+
+  const actions = useMemo(() => {
+    const actionsArray: (SpotlightActionGroupData | SpotlightActionData)[] = [
+      {
+        id: 'settings',
+        label: 'Settings',
+        description: 'Open settings',
+        onClick: () => router.push('/settings'),
+        leftSection: <IconSettings size={24} stroke={1.5} />,
+      },
+    ];
+
+    // Contexts actions
+    const contextActions = contexts.map((context) => ({
       id: `context-${context}`,
-      title: context,
+      label: context,
       description: `View context: ${context}`,
       onClick: () => router.push(`/contexts/view?name=${context}`),
       leftSection: <IconManualGearboxFilled size={24} stroke={1.5} />,
-    });
-  });
+    }));
 
+    if (contextActions.length > 0) {
+      actionsArray.push({
+        group: `Contexts`,
+        actions: contextActions,
+      });
+    }
+
+    // Open tasks actions
+    const openTaskActions = openTasks.map((task) => ({
+      id: `task-${task._id}`,
+      label: task.title,
+      description: task.description,
+      onClick: () => {
+        setSelectedTask(task);
+        open();
+      },
+    }));
+
+    if (openTaskActions.length > 0) {
+      actionsArray.push({
+        group: `Tasks`,
+        actions: openTaskActions,
+      });
+    }
+
+    return actionsArray;
+  }, [contexts, openTasks]);
+
+  // Subscribe to contexts
   useEffect(() => {
     const unsubscribe = dataSource.subscribeToContexts(setContexts);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [dataSource]);
+
+  // Subscribe to open tasks
+  useEffect(() => {
+    const unsubscribe = dataSource.subscribeToTasks(
+      {
+        statuses: [TaskStatus.Started, TaskStatus.Waiting, TaskStatus.Ready],
+      },
+      setOpenTasks
+    );
 
     return () => {
       unsubscribe();
@@ -47,6 +103,9 @@ export default function CommandPalette() {
           placeholder: 'Search...',
         }}
       />
+      <Modal opened={taskOpened} onClose={close} title="Edit Task" size="lg">
+        {selectedTask && <TaskEditor task={selectedTask} handleClose={cancelEditing} />}
+      </Modal>
     </>
   );
 }
